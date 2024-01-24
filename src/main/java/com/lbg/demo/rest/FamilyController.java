79cd592322f5bafd428211ead34e64bc9cecd716
treesacherian <treesa.cherian@gmail.com>
2024-01-24 16:08:05 +0000

    status code Demo

--- src/main/java/com/lbg/demo/rest/FamilyController.java
+++ src/main/java/com/lbg/demo/rest/FamilyController.java
@@ -3,6 +3,8 @@ package com.lbg.demo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,17 +26,25 @@ public class FamilyController {
 	}
 
 	@PostMapping("/create")
-
-	public Family createFamily(@RequestBody Family newFamily) {
-		this.families.add(newFamily);
-//		return newFamily.toString();
-//		to use the above return statem,ent, the return type should be String
-		return families.get(this.families.size() - 1);
+	public ResponseEntity<Family> createFamily(@RequestBody Family family) {
+		this.families.add(family);
+		Family newFamily = this.families.get(this.families.size() - 1);
+		return new ResponseEntity<>(newFamily, HttpStatus.CREATED);
+//		return this.families.get(this.families.size() - 1);
 	}
 
+//	public Family createFamily(@RequestBody Family newFamily) {
+//		this.families.add(newFamily);
+//		return this.families.get(this.families.size() - 1);
+//	}
+
 	@GetMapping("/get/{id}")
-	public Family getFamily(@PathVariable int id) {
-		return this.families.get(id);
+	public ResponseEntity<Family> getFamily(@PathVariable int id) {
+		if (id < 0 || id >= this.families.size())
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		Family member = this.families.get(id);
+//		return ResponseEntity.ok(member);
+		return new ResponseEntity<>(member, HttpStatus.OK);
 	}
 
 	@PutMapping("/update/{id}")
@@ -42,6 +52,7 @@ public class FamilyController {
 	public String updateFamily(@PathVariable int id, @RequestBody Family family) {
 		families.set(id, family);
 		return families.toString();
+
 	}
 
 //	public Family updateFamily(@PathVariable int id, @RequestBody Family family) {
